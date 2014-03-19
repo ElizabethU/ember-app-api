7eae58ee996769823dc3f2d78a4ddbb46abb8393
Elizabeth Uselton <elizabeth.uselton@gmail.com>
2014-03-19 09:57:51 -0700

    item controller methods filled out

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,43 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only [:update, :show, :destroy]
+
+  def create
+    @item = Item.new(item_params)
+
+    if @item.save
+      render json: @item, status: :created
+    else
+      render json: @item.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @item.update(item_params)
+      render "show"
+    else
+      render json: @item.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @item.destroy
+    render json: '', status: :no_content
+  end
 
   def index
     @items = Item.all
   end
 
   def show
+  end
+
+  private
+  
+  def set_item
     @item = Item.find(params[:id])
+  end
+
+  def item_params
+    params.require(:item).permit(:product, :quantity, :current_price)
   end
 end
