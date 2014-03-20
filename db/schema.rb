986f05f79b5ab4893472b39bc385dfa1fc2cbb73
Elizabeth Uselton <elizabeth.uselton@gmail.com>
2014-03-20 10:17:18 -0700

    added cart id to items

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140320171131) do
+ActiveRecord::Schema.define(version: 20140320171628) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20140320171131) do
     t.string   "current_price"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "cart_id"
   end
 
   create_table "orders", force: true do |t|
