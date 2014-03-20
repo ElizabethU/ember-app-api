a66639b75f5c5ebdebbc0e302de238d1501d3356
Elizabeth Uselton <elizabeth.uselton@gmail.com>
2014-03-20 10:10:30 -0700

    filled out cart controller

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,2 +1,43 @@
 class CartsController < ApplicationController
+  before_action :set_cart, only: [:update, :show, :destroy]
+
+  def create
+    @item = Cart.new(cart_params)
+
+    if @cart.save
+      render json: @cart, status: :created
+    else
+      render json: @cart.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @cart.update(cart_params)
+      render "show"
+    else
+      render json: @cart.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @cart.destroy
+    render json: '', status: :no_content
+  end
+
+  def index
+    @carts = Cart.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_cart
+    @cart = Cart.find(params[:id])
+  end
+
+  def item_params
+    params.require(:item).permit(:order_id)
+  end
 end
