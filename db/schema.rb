133a778e3179b8b8168f2290c266993bbe26182b
Elizabeth Uselton <elizabeth.uselton@gmail.com>
2014-03-20 10:15:50 -0700

    added cart id to order model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140320170646) do
+ActiveRecord::Schema.define(version: 20140320171131) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20140320170646) do
     t.string   "zipcode"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "cart_id"
   end
 
   create_table "products", force: true do |t|
